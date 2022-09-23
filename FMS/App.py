7930fa17d5b804a7032f3592ffd0b8bc77f85a31
Xu Min <52066898+TsukiBani@users.noreply.github.com>
2022-09-24 00:49:44 +0800

    Database connected
    connection of database. Able to create employee

--- FMS/App.py
+++ FMS/App.py
@@ -1,22 +1,27 @@
 from logging import exception
-from flask import Flask, render_template,request, redirect, url_for
-from flask_mysqldb import MySQL
+from flask import Flask, render_template,request, redirect, url_for,flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key= "abcd"
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:+pymysql//root:Barney-123@localhost/fmssql.db'
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Barney-123'
-app.config['MYSQL_DB'] = 'fmssql'
-mysql = MySQL(app)
-#Creating a connection cursor
-with app.app_context():
-    cursor = mysql.connection.cursor()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Barney-123@localhost/fmssql'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+    def __init__(self,name,email,phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
 @app.route('/')
 def index():
-    return render_template("index.html")
+    all_data = Data.query.all()
+    return render_template("index.html", employees = all_data)
 
 @app.route('/insert', methods=['POST'])
 def insert():
@@ -24,19 +29,25 @@ def insert():
         name = request.form['name']
         email = request.form['email']
         phone = request.form['phone']
-        try:
-            cursor.execute('USE fmssql')
-            cursor.execute('''INSERT INTO test (name,email,phone)VALUES (%s,%s,%s)''',(name,email,phone))
-            results = cursor.fetchall()
-            mysql.connection.commit()
-            cursor.close()
-            return "NOOOO"
+        
+        my_data = Data(name,email,phone)
+        db.session.add(my_data)
+        db.session.commit()
+        flash("Employee Inserted Sucessfully")
+        return redirect(url_for('index'))
 
-        except:
-            return exception
+# @app.route('/update', methods=['GET','POST'])
+# def update():
+#     if request.method == 'POST':
+#         my_data = Data.query.get(request.form.get('id'))
+#         my_data.name = request.form['name']
+#         my_data.email = request.form['email']
+#         my_data.phone = request.form['name']
+#         print("Testing Data Output = " +my_data.name, my_data.email, my_data.phone)
+#         db.session.commit()
 
-    else:
-        return "Nil"
+#         flash("Employee Updated Sucessfully")
+#         return redirect(url_for('index'))
 if __name__ == "__main__":
     app.run(debug=True)
 
