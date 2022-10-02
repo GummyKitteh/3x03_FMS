33e2ed2b4cd7a34f41a6b2b93a005a6874ada61d
Jaspzx <jaspzx@hotmail.com>
2022-10-02 18:44:09 +0800

    Create Employee Branch

--- FMS/App.py
+++ FMS/App.py
@@ -3,14 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    login_required,
+    logout_user,
+    current_user,
+)
 
 app = Flask(__name__)
 app.secret_key = "abcd"
-app.config['SECRET_KEY'] ="I really hope fking this work if never idk what to do :("
+app.config["SECRET_KEY"] = "I really hope fking this work if never idk what to do :("
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Barney-123@localhost/fmssql'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:qwerty1234@localhost/fmssql'
-#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:B33pb33p!@178.128.17.35/fmssql"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:Barney-123@localhost/fmssql"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:qwerty1234@localhost/fmssql"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:B33pb33p!@178.128.17.35/fmssql"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
@@ -26,21 +34,28 @@ class Data(db.Model):
         self.name = name
         self.email = email
         self.phone = phone
+
+
 class datainsert(FlaskForm):
-    name = StringField("Name",[DataRequired(),Length(max=50)])
-    email = StringField("Email",[DataRequired(),Email(), Length(max=100)])
-    phone = StringField("Phone",[DataRequired(), Length(min=8), Length(max=8)])
-    submit = SubmitField("Submit",[DataRequired()])
+    name = StringField("Name", [DataRequired(), Length(max=50)])
+    email = StringField("Email", [DataRequired(), Email(), Length(max=100)])
+    phone = StringField("Phone", [DataRequired(), Length(min=8), Length(max=8)])
+    submit = SubmitField("Submit", [DataRequired()])
+
 
 @app.route("/")
 def index():
     all_data = Data.query.all()
     form = datainsert()
     return render_template("index.html", employees=all_data)
+
+
 @app.context_processor
 def index():
     form = datainsert()
     return dict(form=form)
+
+
 @app.route("/insert", methods=["POST"])
 def insert():
     form = datainsert()
@@ -51,9 +66,9 @@ def insert():
         name = form.name.data
         email = form.email.data
         phone = form.phone.data
-        form.name.data = ''
-        form.email.data = ''
-        form.phone.data = ''
+        form.name.data = ""
+        form.email.data = ""
+        form.phone.data = ""
         my_data = Data(name, email, phone)
         db.session.add(my_data)
         db.session.commit()
@@ -61,8 +76,7 @@ def insert():
         return redirect("/")
 
 
-
-@app.route("/update", methods=["GET","POST"])
+@app.route("/update", methods=["GET", "POST"])
 def update():
     if request.method == "POST":
         my_data = Data.query.get(request.form.get("id"))
@@ -95,6 +109,15 @@ def login():
 @app.route("/reset")
 def reset():
     return render_template("reset.html")
+
+
+# class Employee(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     # username = db.Column(db.String())
+#     username = db.Column(db.String(50), nullable=False)
+#     email = db.Column(db.String(100), nullable=False)
+#     contactNumber = db.Column(db.Integer, nullable=False)
+#     password_hash = db.Column(db.String(64), nullable=False)
 
 
 if __name__ == "__main__":
