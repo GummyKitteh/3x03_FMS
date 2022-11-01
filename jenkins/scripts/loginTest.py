6e667b1a1447e27f08579cd3ba164f0fc7683b52
Kho Wen Jie <gummykitteh@gmail.com>
2022-11-01 13:45:37 +0800

    update test script

--- jenkins/scripts/loginTest.py
+++ jenkins/scripts/loginTest.py
@@ -2,11 +2,12 @@ import pytest
 from flask.testing import FlaskClient
 from src.app import server, db
 
-
 @pytest.fixture(scope='module')
 def flask_app():
     app = server
     with app.app_context():
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['TESTING'] = True
         yield app
 
 
@@ -24,4 +25,22 @@ def test_index_page(client):
     assert res.status_code == 200
 
 
-#def test_login_page(client):
+def test_login_page(client):
+    res = client.get('/login')
+    assert res.status_code == 200
+
+
+def test_login_success(client):
+    response = client.post(
+        "/login",
+        data=dict(Email="b33p33p@gmail.com", password="admin@123", submit="Login"),
+        follow_redirects=True)
+    assert response.status_code == 302
+
+
+def test_login_failure(client):
+    response = client.post(
+        "/login",
+        data=dict(Email="wrong@gmail.com", password="wrong", submit="Login"),
+        follow_redirects=True)
+    assert response.status_code == 200
