2ba3323ac11d7f59df33c107e9564b4c6c46a586
Lim Yue Heng <lim_yue_heng@hotmail.com>
2022-11-06 05:46:41 +0800

    Removed unnecessary firewall commands from startup/stop scripts

--- architecture/stop_docker.sh
+++ architecture/stop_docker.sh
@@ -3,13 +3,3 @@ docker compose rm -fs
 docker compose down
 service mysql stop
 service nginx stop
-sudo ufw-docker delete allow Flask4Nginx 5000
-sudo ufw-docker delete allow Nginx4Flask 80
-sudo ufw-docker delete allow Nginx4Flask 443
-sudo ufw-docker delete allow jenkins 8080
-sudo ufw-docker delete allow jenkins 50000
-sudo ufw-docker delete allow docker 3000
-sudo ufw-docker delete allow docker 5555
-sudo ufw-docker delete allow docker 2376
-sudo ufw disable
-sudo systemctl restart ufw
